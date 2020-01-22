b91c1a5e30eaf2e8b7e01ea800757315ebae6939
user1 <user1@localhost>
2020-01-22 17:22:45 +0530

    FormDatabase added1

--- FormDatabse/home.php
+++ FormDatabse/home.php
@@ -1,6 +1,6 @@
 <?php
 try{
-	$dbhandler = new PDO('mysql:host=127.0.0.1;dbname=cetvr','cetvr','cetvr');
+	$dbhandler = new PDO('mysql:host=127.0.0.1;dbname=ce4_49','ce4_49','ce4_49');
 	echo "Connection is established...<br/>";
 	$dbhandler->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 	if(isset($_POST['username']) && isset($_POST['password']))
@@ -9,7 +9,7 @@ try{
 		$password=$_POST['password'];
 		if(!empty($username) && !empty($password))
 			{
-	$sql="insert into student (name,password,gender,birthdate,achivments) values (?,?,?,?,?)";
+	$sql="insert into login (user,password,gender,bday,message) values (?,?,?,?,?)";
 	$query=$dbhandler->prepare($sql);
 	//bind and execute directly
 	$query->execute(array($_POST['username'],$_POST['password'],$_POST['gender'],$_POST['date'],$_POST['achievement']));
